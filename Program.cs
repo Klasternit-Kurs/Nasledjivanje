04e0a7e5045501d5c4564ccd8f4d49fb0539ece2
Dragan Pepic <d.pepic@hotmail.com>
2020-02-19 20:02:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledjivanje
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Primer p = new Primer(6, "asd");
+
+
+			object o = new object();
+			int a = 5;
+			Nesto x = new Nesto("asdasdasd", 5);
+		}
+	}
+
+	class Primer
+	{
+		public int Broj;
+
+		public Primer(int i, string nn)
+		{
+			Broj = i;
+		}
+	}
+
+	class Nesto : Primer
+	{
+		public string Test;
+		public Nesto(string t, int n) : base(n, "asd")
+		{
+			Test = t;
+		}
+	}
+}
